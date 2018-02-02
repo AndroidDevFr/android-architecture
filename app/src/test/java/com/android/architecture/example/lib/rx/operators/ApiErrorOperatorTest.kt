b37609dd22f6dd2d75946b41491e255706e23bf7
Guillaume M <nodens.gm@gmail.com>
2018-02-02 09:31:14 +0000

    fix api error operator unit test

--- app/src/test/java/com/android/architecture/example/lib/rx/operators/ApiErrorOperatorTest.kt
+++ app/src/test/java/com/android/architecture/example/lib/rx/operators/ApiErrorOperatorTest.kt
@@ -1,6 +1,7 @@
 package com.android.architecture.example.lib.rx.operators
 
 import com.android.architecture.example.SampleRobolectricTestCase
+import com.android.architecture.example.network.apiresponses.ErrorEnvelope
 import com.android.architecture.example.network.exceptions.ApiException
 import com.google.gson.Gson
 import io.reactivex.subjects.PublishSubject
@@ -17,16 +18,20 @@ class ApiErrorOperatorTest : SampleRobolectricTestCase() {
     fun testErrorResponse() {
         val gson = Gson()
 
+        val error: PublishSubject<ErrorEnvelope> = PublishSubject.create()
         val response: PublishSubject<Response<Int>> = PublishSubject.create()
         val result = response.lift(Operators.apiError(gson))
 
+        val resultError = TestSubscriber<ErrorEnvelope>()
         val resultTest = TestSubscriber<Int>()
         result.subscribe(resultTest::onNext)
+        error.subscribe(resultError::onNext)
 
         response.onNext(Response.error<Int>(400, ResponseBody.create(MediaType.parse("application/json"),
                         "{\"status\": \"KO\",\"error\": \"bad request\"}")))
 
         resultTest.assertNoValues()
+        resultError.assertValueCount(1)
     }
 
 }
